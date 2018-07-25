8eb3b84e717242c2bf0f7ddddc6df06561d7e80e
nowak <nowak.vincent@gmail.com>
2018-07-25 23:39:59 +0200

    On ajoute des sauts de ligne et une phrase de fin

--- monfichier.php
+++ monfichier.php
@@ -1,6 +1,12 @@
 <?php
 //Vincent: On dit bonjour à Openclassrooms
 echo("Bonjour Openclassrooms");
+echo("</br>");
 
+//Vincent: On parle de vim
 echo("Grâce à vim, je peux modifier mon fichier dans la console, c'est top!");
+echo("</br>");
+
+//Vincent: Dernière ligne de mon fichier.php
+echo("Fin");
 ?>
